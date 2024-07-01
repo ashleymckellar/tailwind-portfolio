e9c528f55f24a6533740797abe6dee144f49177b
Ashley McKellar <ashley.l.mckellar@gmail.com>
2024-06-30 21:53:45 -0700

    updated summary

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -45,7 +45,7 @@ const Summary = () => {
     return (
         <div id="about" className="flex justify-center px-4 sm:px-6 lg:px-8">
             <p className="mb-20 text-lg sm:text-xl text-dark-grey text-center w-full max-w-7xl bg-white py-6 sm:py-10 rounded-md shadow-lg bg-rosy mx-4 sm:mx-6 lg:mx-8">
-                Hi, I'm Ashley and I'm a Seattle-area software developer
+                Hi, I'm Ashley. I'm a Seattle-area software developer
                 looking to make an impact on your team. With a background in
                 full-stack software development utilizing the MERN stack, I've
                 also developed apps using Firebase and Supabase, and I also have
